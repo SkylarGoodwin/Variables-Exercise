d0cedced37441db8b692e9d46d1ccac280624ab3
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-02-03 06:29:29 -0600

    lets hope this works

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+string Name = "Skylar";
+int Age = 25;
+char Currency = '$';
+bool Paid = true;
+double Speed = 69.420;
+decimal Price = 420.69m;
+
+Console.WriteLine($"My name is {Name}, i am {Age} years old");
+Console.WriteLine($"{Currency}{Price}");
+Console.WriteLine($"Paid is {Paid}");
+Console.WriteLine($"Speed is {Speed}");
